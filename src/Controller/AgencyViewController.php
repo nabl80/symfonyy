a6eac348e9c8aa81d4983ec2e932bc15875d17fc
Vardenis Pavardenis <v.pavardenis@pastas.lt>
2021-08-07 10:49:26 +0300

    before paginator

--- src/Controller/AgencyViewController.php
+++ src/Controller/AgencyViewController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\News;
 use App\Entity\Property;
 use App\Repository\NewsRepository;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +13,8 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/aview')]
 class AgencyViewController extends AbstractController
 {
+
+
     #[Route('/', name: 'agency_view_index', methods: ['GET'])]
     public function list(NewsRepository $newsRepository): Response
     {
@@ -20,7 +23,23 @@ class AgencyViewController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'agency_view_show', methods: ['GET'])]
+    #[Route('/properties', name: 'agency_view_property_list', methods: ['GET'])]
+    public function index(PropertyRepository $propertyRepository): Response
+    {
+        return $this->render('agencyView/agency.view.property.list.html.twig', [
+            'properties' => $propertyRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/properties/{id}', name: 'agency_view_property_show', methods: ['GET'])]
+    public function show1(Property $property): Response
+    {
+        return $this->render('agencyView/agency.view.property.show.html.twig', [
+            'property' => $property,
+        ]);
+    }
+
+    #[Route('/news/{id}', name: 'agency_view_news_show', methods: ['GET'])]
     public function show(News $news): Response
     {
         return $this->render('agencyView/agency.view.news.show.html.twig', [
@@ -28,18 +47,4 @@ class AgencyViewController extends AbstractController
         ]);
     }
 
-    #[Route('/properties', name: 'property_list', methods: ['GET'])]
-    public function index(Property $property): Response
-    {
-        return $this->render('agencyView/agency.view.property.index.html.twig', [
-            'property' => $property,
-        ]);
-    }
-
-    public function indexAction(){
-        $em = $this->get('doctrine')->getManager();
-        $sections = $sections = $this->getDoctrine() ->getRepository('AppBundle:ForumSection') ->findAll();
-
-        return $this->render('AppBundle:Default:index.html.twig', array('sections'=>$sections));
-    }
 }
